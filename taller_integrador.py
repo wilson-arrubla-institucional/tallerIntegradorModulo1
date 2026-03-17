653b331e03520983253af5d48c1ca0a032f45af4
wilson <wilson.arrubla@unad.edu.co>
2026-03-16 20:59:19 -0500

    Se realiza ajuste para controlar el rango de entrada de las notas para que esten dentro de 0 y 5

--- taller_integrador.py
+++ taller_integrador.py
@@ -25,6 +25,7 @@ def determinar_estado(promedio):
 def menu(): 
     estudiantes = []
     while True:
+        print("\n========Sistema de Gestión Básica de Estudiantes========\n")
         print("1. Registrar estudiante")
         print("2. Calcular promedio y estado")
         print("3. Salir")
@@ -34,20 +35,33 @@ def menu():
             nombre = input("Ingrese el nombre del estudiante: ")
             edad = int(input("Ingrese la edad del estudiante: "))
             nota1 = float(input("Ingrese la nota 1: "))
+            # revisar que las notas estén entre 0 y 5
+            while nota1 < 0 or nota1 > 5:
+                print("La nota debe estar entre 0 y 5. Por favor, ingrese una nota válida.")
+                nota1 = float(input("Ingrese la nota 1: "))
+                
             nota2 = float(input("Ingrese la nota 2: "))
+            while nota2 < 0 or nota2 > 5:
+                print("La nota debe estar entre 0 y 5. Por favor, ingrese una nota válida.")
+                nota2 = float(input("Ingrese la nota 2: "))
             nota3 = float(input("Ingrese la nota 3: "))
+            while nota3 < 0 or nota3 > 5:
+                print("La nota debe estar entre 0 y 5. Por favor, ingrese una nota válida.")
+                nota3 = float(input("Ingrese la nota 3: "))
+
             estudiante = registrar_estudiantes(nombre, edad, nota1, nota2, nota3)
             estudiantes.append(estudiante)
-            print("Estudiante registrado exitosamente.")
+            print("\nEstudiante registrado exitosamente.\n")
         
         elif opcion == "2":
+            print("\n==========Resultados de los estudiantes:==========\n")
             for estudiante in estudiantes:
                 promedio = calcular_promedio(estudiante["nota1"], estudiante["nota2"], estudiante["nota3"])
                 estado = determinar_estado(promedio)
                 print(f"Estudiante: {estudiante['nombre']}, Promedio: {promedio:.2f}, Estado: {estado}")
         
         elif opcion == "3":
-            print("Saliendo del programa.")
+            print("Saliendo del Sistema...")
             break
         
         else:
